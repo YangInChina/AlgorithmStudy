36a7d8b38c6c4563160d719c5e881ebb12a562c9
BoYang_Zhang <91548907+YangInChina@users.noreply.github.com>
2022-01-16 20:50:56 +0800

    Update majorityElement.c

--- week1/ch03/majorityElement.c
+++ week1/ch03/majorityElement.c
@@ -13,8 +13,3 @@ public:
         return majority;
     }
 };
-
-作者：LeetCode-Solution
-链接：https://leetcode-cn.com/problems/majority-element/solution/duo-shu-yuan-su-by-leetcode-solution/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
