0b76dde85bdbe330d519d65acd22955d8bdf55b5
BoYang_Zhang <91548907+YangInChina@users.noreply.github.com>
2022-01-16 20:50:42 +0800

    Create majorityElement.c

---
+++ week1/ch03/majorityElement.c
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> counts;
+        int majority = 0, cnt = 0;
+        for (int num: nums) {
+            ++counts[num];
+            if (counts[num] > cnt) {
+                majority = num;
+                cnt = counts[num];
+            }
+        }
+        return majority;
+    }
+};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/majority-element/solution/duo-shu-yuan-su-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
